cf2d4ea17ae578756e51ab525bc557747babdb98
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-30 12:48:55 +0300

    event handler created for adding friend

--- src/components/AddFriendForm.jsx
+++ src/components/AddFriendForm.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
-const AddFriendForm = () => {
+const AddFriendForm = ({ handleAddFriend }) => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const userId = crypto.randomUUID();
+
+    if (!name || !image) return;
+
+    const newFriend = {
+      name,
+      image: `${image}?u=${userId}`,
+      balance: 0,
+      id: userId,
+    };
+
+    handleAddFriend(newFriend);
+    setName('');
+    setImage('https://i.pravatar.cc/48');
+  };
+
   return (
-    <form className='form-add-friend'>
+    <form className='form-add-friend' onSubmit={handleSubmit}>
       <label htmlFor='friend-name'>🧙🏻‍♂️Friend Name</label>
-      <input id='friend-name' type='text' />
+      <input value={name} onChange={e => setName(e.target.value)} id='friend-name' type='text' />
 
       <label htmlFor='image'>🎇Image Url</label>
-      <input id='image' type='text' />
+      <input value={image} onChange={e => setImage(e.target.value)} id='image' type='text' />
 
       <Button>Add</Button>
     </form>
