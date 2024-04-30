9db86bbfd8d6ab10c1a3acea0aaedddcaacbe4d7
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-30 15:36:42 +0300

    fixed mapping bugs

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -9,28 +9,28 @@ export default FriendsList;
 
 // tek friend renderlemak icin
 export const Friend = ({ friend, handleSelectedFriend, selectedFriend }) => {
-  const isSelected = friend.id === selectedFriend?.id;
+  //
+  const isSelected = selectedFriend?.id === friend.id;
+
   return (
     <>
-      {friend.map(friend => (
-        <li key={friend.id} className={isSelected ? 'selected' : ''}>
-          <img src={friend.image} alt={friend.name} />
-          <h3>{friend.name}</h3>
+      <li key={friend.id} className={isSelected ? 'selected' : ''}>
+        <img src={friend.image} alt={friend.name} />
+        <h3>{friend.name}</h3>
 
-          {friend.balance < 0 ? (
-            <p className='red'>
-              You owe {friend.name} ${Math.abs(friend.balance)}
-            </p>
-          ) : friend.balance > 0 ? (
-            <p className='green'>
-              {friend.name} owe you ${friend.balance}
-            </p>
-          ) : (
-            <p>You and {friend.name} are even</p>
-          )}
-          <Button onClick={() => handleSelectedFriend(friend)}>{isSelected ? 'Close' : 'Select'}</Button>
-        </li>
-      ))}
+        {friend.balance < 0 ? (
+          <p className='red'>
+            You owe {friend.name} ${Math.abs(friend.balance)}
+          </p>
+        ) : friend.balance > 0 ? (
+          <p className='green'>
+            {friend.name} owe you ${friend.balance}
+          </p>
+        ) : (
+          <p>You and {friend.name} are even</p>
+        )}
+        <Button onClick={() => handleSelectedFriend(friend)}>{isSelected ? 'Close' : 'Select'}</Button>
+      </li>
     </>
   );
 };
